8a9589eb48f4643d2fcc6208712d7e4701dd8640
shagun <shagun@gmail.com>
2025-05-25 11:15:42 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
@@ -14,18 +13,17 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
 const corsOptions = {
-    origin:true,
-    credentials:true
+    origin: true,
+    credentials: true
 }
 
 //database connection
-mongoose.set("strictQuery" , false);
-const connect = async() =>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI,{
-        })
+mongoose.set("strictQuery", false);
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {})
         console.log('MongoDB database connected');
-    }catch(err){
+    } catch (err) {
         console.log('MongoDB connection failed');
     }
 }
@@ -34,13 +32,16 @@ const connect = async() =>{
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use('/api/v1/auth',authRoute);
-app.use('/api/v1/tours',tourRoute);
-app.use("/api/v1/users",userRoute);
-app.use("/api/v1/review",reviewRoute)
-app.use("/api/v1/booking",bookingRoute)
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/tours', tourRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/review", reviewRoute)
+app.use("/api/v1/booking", bookingRoute)
 
-app.listen(port, ()=>{
-    connect();
-    console.log('server listening on port',port);
-})
+app.get("/", (req, res) => {
+    res.send("API is running.");
+});
+
+connect();
+
+export default app;

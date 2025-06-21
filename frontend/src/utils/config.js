5678bdbfdfccfb4edada0d578e1c3b0fd44fef9a
shagun <shagun@gmail.com>
2025-06-21 18:07:01 +0530

    change in config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,5 @@
 
-export const BASE_URL = import.meta.env.BACKEND_URL;
+// export const BASE_URL = 'http://localhost:4000/api/v1'
+// export const BASE_URL = import.meta.env.BACKEND_URL;
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+console.log("BASE_URL:", import.meta.env.VITE_BACKEND_URL);

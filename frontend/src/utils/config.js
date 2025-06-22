98750cf2fd00553159843739a1062fdafe02cc17
guptashivam-9370 <shivamg9425@gmail.com>
2025-06-22 12:41:31 +0530

    changed frontend .env

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,5 @@
 
 // export const BASE_URL = 'http://localhost:4000/api/v1'
-// export const BASE_URL = import.meta.env.BACKEND_URL;
-export const BASE_URL = process.env.VITE_BACKEND_URL;
-console.log("BASE_URL:", process.env.VITE_BACKEND_URL);
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+// export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+console.log("BASE_URL:", process.env.REACT_APP_BACKEND_URL);
